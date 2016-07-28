56a96b2670a3b04ab8ee7336944810f5ac619e33
Samuel Greene <sgreene@smartprocure.us>
2016-07-28 17:57:14 +0200

    update lodash

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var metagen = dir => filesRelative(dir.path, dir.exclusions || [dir.output || '_
 metagen.formats = {};
 metagen.formats.commonJS = files => `define(function(require) {
     return {
-        ${ files.map(file => `'${ noExt(file) }': require('./${ noExt(file) }')`).join(',\n        ') }
+        ${ files.map(noExt).map(file => `'${ file }': require('./${ file }')`).join(',\n        ') }
     };
 });`;
 metagen.formats.amd = files => `define([
@@ -30,18 +30,15 @@ metagen.formats.amd = files => `define([
     }
 });`;
 
-// use FP when https://github.com/lodash/lodash/pull/2503 is released
-var zipObjectDeep = require('lodash/zipObjectDeep');
-
 // Deep Formats
 var deepKeys    = _.map(_.flow(noExt, _.replace(/\//g, '.'))),
     stringify   = x => JSON.stringify(x, null, 4),
     indent      = _.replace(/\n/g, '\n    '),
     unquote     = _.replace(/"require(.*)'\)"/g, "require$1')"),
-    deepify     = _.flow(zipObjectDeep, stringify, indent, unquote);
+    deepify     = _.flow(_.zipObjectDeep, stringify, indent, unquote);
     
 metagen.formats.deepCommonJS = files => `define(function(require) {
-    return ${ deepify(deepKeys(files), files.map(file => `require('${ noExt(file) }')`))};
+    return ${ deepify(deepKeys(files), files.map(file => `require('./${ noExt(file) }')`))};
 });`;
 metagen.formats.deepAMD = files => `define([
     ${ files.map(file => `'${ noExt(file) }'`).join(',\n    ') }
