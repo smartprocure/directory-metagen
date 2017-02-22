90f67d944e68e504c62354aa1a249c2dbdbf2ad6
Daniel Rodríguez <sadasant@gmail.com>
2017-02-22 22:16:07 +0000

    ESLint standard ✨

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
 // Inspired by (but complete rewrite of) requirejs-metagen
-let _           = require('lodash/fp'),
-    Promise     = require('bluebird'),
-    readDir     = Promise.promisify(require('recursive-readdir')),
-    fs          = Promise.promisifyAll(require('fs'));
+let _ = require('lodash/fp')
+let Promise = require('bluebird')
+let readDir = Promise.promisify(require('recursive-readdir'))
+let fs = Promise.promisifyAll(require('fs'))
 
 // Path Utils
-let filesRelative = (dir, ex) => readDir(dir, ex).map(x => x.slice(dir.length));
-let noExt = file => file.slice(0, _.lastIndexOf('.', file));
-let test = x => y => x.test(y);
+let filesRelative = (dir, ex) => readDir(dir, ex).map(x => x.slice(dir.length))
+let noExt = file => file.slice(0, _.lastIndexOf('.', file))
+let test = x => y => x.test(y)
 
 // Core
-let filter = _.filter(test(/.js|.html|.jsx|.ts|.coffee|.less|.css|.sass|.hbs|.ejs/));
+let filter = _.filter(test(/.js|.html|.jsx|.ts|.coffee|.less|.css|.sass|.hbs|.ejs/))
 let metagen = dir => filesRelative(dir.path, dir.exclusions || [dir.output || '__all.js'])
     .then(dir.filter || filter)
-    .then(files => fs.writeFileAsync(dir.path + (dir.output || '__all.js'), dir.format(files, dir)));
+    .then(files => fs.writeFileAsync(dir.path + (dir.output || '__all.js'), dir.format(files, dir)))
 
 // Formats
-metagen.formats = {};
+metagen.formats = {}
 metagen.formats.commonJS = files => `define(function(require) {
     return {
-        ${ files.map(noExt).map(file => `'${ file }': require('./${ file }')`).join(',\n        ') }
+        ${files.map(noExt).map(file => `'${file}': require('./${file}')`).join(',\n        ')}
     };
-});`;
+});`
 metagen.formats.amd = files => `define([
-    ${ files.map(file => `'${ noExt(file) }'`).join(',\n    ') }
+    ${files.map(file => `'${noExt(file)}'`).join(',\n    ')}
 ], function() {
     return {
-        ${ files.map((file, i) => `'${ noExt(file) }': arguments[${ i }]`).join(',\n        ') }
+        ${files.map((file, i) => `'${noExt(file)}': arguments[${i}]`).join(',\n        ')}
     }
-});`;
+});`
 
 // Deep Formats
-let deepKeys    = _.map(_.flow(noExt, _.replace(/\//g, '.'))),
-    stringify   = x => JSON.stringify(x, null, 4),
-    indent      = _.replace(/\n/g, '\n    '),
-    unquote     = _.replace(/"require(.*)'\)"/g, "require$1')"),
-    deepify     = _.flow(_.zipObjectDeep, stringify, indent, unquote);
+let deepKeys = _.map(_.flow(noExt, _.replace(/\//g, '.')))
+let stringify = x => JSON.stringify(x, null, 4)
+let indent = _.replace(/\n/g, '\n    ')
+let unquote = _.replace(/"require(.*)'\)"/g, "require$1')")
+let deepify = _.flow(_.zipObjectDeep, stringify, indent, unquote)
 
 metagen.formats.deepCommonJS = files => `define(function(require) {
-    return ${ deepify(deepKeys(files), files.map(file => `require('./${ noExt(file) }')`))};
-});`;
+    return ${deepify(deepKeys(files), files.map(file => `require('./${noExt(file)}')`))};
+});`
 metagen.formats.deepAMD = files => `define([
-    ${ files.map(file => `'${ noExt(file) }'`).join(',\n    ') }
+    ${files.map(file => `'${noExt(file)}'`).join(',\n    ')}
 ], function() {
-    return ${ deepify(deepKeys(files), files.map((file, i) => `arguments[${ i }]`))};
-});`;
+    return ${deepify(deepKeys(files), files.map((file, i) => `arguments[${i}]`))};
+});`
 
-module.exports = metagen;
+module.exports = metagen
